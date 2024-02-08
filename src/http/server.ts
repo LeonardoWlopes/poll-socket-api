5c79492ab1201a27c4d427ebfe7453dc46a156cf
Leonardo William Lopes <leonardolopesdeveloper@gmail.com>
2024-02-07 23:50:58 -0300

    initial commit

---
+++ src/http/server.ts
@@ -0,0 +1,30 @@
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+
+import cookie from "@fastify/cookie";
+import fastifyWebsocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
+import { env } from "./env";
+
+const app = fastify();
+
+app.register(cookie, {
+	secret: "my-secret",
+	hook: "onRequest",
+});
+
+app.register(fastifyWebsocket);
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+
+app.register(pollResults);
+
+app.listen({ port: +env.PORT }).then((url) => {
+	console.log(`🚀 Server is running on ${url}`);
+});
